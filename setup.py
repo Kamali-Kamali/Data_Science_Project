c63d6c5fa6792877c84232df76ceaf60112d1249
Kamali-Kamali <kamalima12002@gmail.com>
2025-01-06 22:37:38 -0800

    setup file added

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import find_packages, setup
+from typing import List
+
+HYPEN_E_DOT='-e .'
+def get_requirements(file_path:str)->List[str]:
+    '''
+    this is a function to read and install requirements inside requirements.txt , it will return the list of requirements'''
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
+
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+
+    return requirements
+
+
+
+setup(
+
+    name='project setup',
+    version='0.0.1',
+    author='kamali',
+    author_email='kamalima12002@gmail.com',
+    packages=find_packages(),
+    #install_requires=['pandas','numpy','seaborn']  
+    install_requires=get_requirements('requirements.txt')
+
+)
